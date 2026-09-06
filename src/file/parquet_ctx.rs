60e34575c95197ab9ad6109907244e396c6087b1
Kaushik Srinivasan <kaushik.srinii@gmail.com>
2026-09-05 22:01:59 -0700

    nit: cargo fmt

--- src/file/parquet_ctx.rs
+++ src/file/parquet_ctx.rs
@@ -55,12 +55,11 @@ impl ParquetCtx {
             details: format!("Failed to parse schema: {e}"),
         })?;
 
-        let sample_data =
-            ParquetSampleData::open(file_path, metadata.num_rows).map_err(|e| {
-                FileIOError::SampleDataError {
-                    details: e.to_string(),
-                }
-            })?;
+        let sample_data = ParquetSampleData::open(file_path, metadata.num_rows).map_err(|e| {
+            FileIOError::SampleDataError {
+                details: e.to_string(),
+            }
+        })?;
 
         Ok(ParquetCtx {
             file_path: file_path.to_string(),
